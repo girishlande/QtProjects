53ff096458e49e85333ea34d1ff5a75c31a858b1
AGILiAD\Girish.Lande <girish.lande@agiliad.com>
2020-03-06 14:53:38 +0530

    Added splash screen demo

---
+++ splashdemo/main.cpp
@@ -0,0 +1,26 @@
+#include "mainwindow.h"
+#include <QApplication>
+#include <qsplashscreen.h>
+#include <chrono>
+#include <thread>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    QPixmap pixmap(":/splash_vision.bmp");
+    QSplashScreen *splash = new QSplashScreen(pixmap);
+    splash->show();
+    splash->showMessage("Loaded modules");
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    app.processEvents();
+
+    splash->showMessage("Established connections");
+    app.processEvents();
+
+    splash->hide();
+    QMainWindow window;
+    window.showMaximized();
+
+    return app.exec();
+}
